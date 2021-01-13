4067e59a82851b1de39c1dbe4135ec59d1ef3364
Gwendal <gwendou95@hotmail.fr>
2021-01-13 23:17:26 +0100

    📝Change comment

--- src/main/java/fr/esgi/archi/FileManager.java
+++ src/main/java/fr/esgi/archi/FileManager.java
@@ -12,8 +12,6 @@ import java.nio.file.StandardCopyOption;
  * - Distribution des fichiers sur workers génériques
  * - Une fois le fichier traiter l'envoie dans un autre répertoire
  * - Gestion du ACK (du retour du worker)
- * - (option) Vérifie les extensions des fichiers
- * pour les distribuer sur des workers specialisés (.txt, .md) sinon worker gen
  * - (option) Si dossier reprendre le contenue de celui-ci et (File.isDirectory())
  * extraire dans le directory courant
  */
